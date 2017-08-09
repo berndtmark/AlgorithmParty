2954d46b417ba173eeb540003efbef9d5a29c218
Mark Berndt <berndtmark@gmail.com>
2017-08-09 20:16:17 +0100

    fluent builder pattern

---
+++ AlgorithmParty/Patterns/Fluent Builder/TeamApp.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmParty.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmParty.Patterns.Fluent_Builder
+{
+    public class TeamApp
+    {
+        public void Execute()
+        {
+            TeamBuilder tb = new TeamBuilder();
+
+            Team team3 = tb.CreateTeam("Chelsea")
+                .WithNickName("The blues")
+                .WithShirtColor(Color.Blue)
+                .FromTown("London")
+                .PlayingAt("Stamford Bridge");
+        }
+    }
+}
---
+++ AlgorithmParty/Patterns/Fluent Builder/TeamBuilder.cs
@@ -0,0 +1,58 @@
+﻿using AlgorithmParty.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmParty.Patterns.Fluent_Builder
+{
+    public class TeamBuilder
+    {
+        private string name;
+        private string nickName;
+        private Color shirtColor;
+        private string homeTown;
+        private string ground;
+
+        public TeamBuilder CreateTeam(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public TeamBuilder WithNickName(string nickName)
+        {
+            this.nickName = nickName;
+            return this;
+        }
+
+        public TeamBuilder WithShirtColor(Color shirtColor)
+        {
+            this.shirtColor = shirtColor;
+            return this;
+        }
+
+        public TeamBuilder FromTown(string homeTown)
+        {
+            this.homeTown = homeTown;
+            return this;
+        }
+
+        public TeamBuilder PlayingAt(string ground)
+        {
+            this.ground = ground;
+            return this;
+        }
+
+        public static implicit operator Team(TeamBuilder tb)
+        {
+            return new Team(
+                tb.name,
+                tb.nickName,
+                tb.shirtColor,
+                tb.homeTown,
+                tb.ground);
+        }
+    }
+}
