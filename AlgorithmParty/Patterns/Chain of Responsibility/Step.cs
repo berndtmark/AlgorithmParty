ae5beb4a8c41c4eae1b3e0467a38eff5e5aa6fb7
berndtmark <berndtmark@gmail.com>
2018-06-11 14:00:22 +0100

    chain of responsibility

---
+++ AlgorithmParty/Patterns/Chain of Responsibility/Step.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmParty.Patterns.Chain_of_Responsibility
+{
+    public abstract class Step
+    {
+        private Step _nextStep;
+
+        protected abstract void Process(object param);
+
+        public virtual void Next(Step step)
+        {
+            _nextStep = step;
+        }
+
+        public void Execute(object param)
+        {
+            Process(param);
+
+            _nextStep?.Process(param);
+        }
+    }
+}
---
+++ AlgorithmParty/Patterns/Chain of Responsibility/StepBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmParty.Patterns.Chain_of_Responsibility
+{
+    public class StepBuilder
+    {
+        private readonly IList<Step> _steps;
+        private readonly object _params;
+
+        public StepBuilder(object param)
+        {
+            _params = param;
+            _steps = new List<Step>();
+        }
+
+        public StepBuilder Add(Step step)
+        {
+            _steps.Add(step);
+
+            return this;
+        }
+
+        public StepBuilder Run()
+        {
+            foreach (var step in _steps)
+            {
+                step.Execute(_params);
+            }
+
+            return this;
+        }
+    }
+}
